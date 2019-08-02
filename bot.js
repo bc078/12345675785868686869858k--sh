e4886bde131c4b0b273b2376016a4588069d2133
bc078 <52862281+bc078@users.noreply.github.com>
2019-08-02 15:54:32 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,12 @@
 
+Client.on('guildMemberAdd', async member => {
+    const channel = member.guild.channels.find(ch => ch.name === 'member-log');
+    if (!channel) return;
+
+    channel.send(`**Welcome to Pluie.**, ${member}`)
+})
+Client.on('message', async message => {
+	if (message.content === '!join') {
+		Client.emit('guildMemberAdd', message.member || await message.guild.fetchMember(message.author));
+	}
+});
